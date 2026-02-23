14c4462467a2fa032b563471af71bb4043c476fc
theodormv <theodor.mikal.vogt@vikenfiber.no>
2026-02-23 15:35:14 +0100

    changed prob function with global k, implementet 3a

--- oppgaver/oppgave_3.py
+++ oppgaver/oppgave_3.py
@@ -1,6 +1,8 @@
 import scipy as sp
 import numpy as np
 
+from utilities.probs import p_minus, p_plus
+
 
 
 alpha = 0.8
@@ -8,6 +10,7 @@ T = 273.15 + 37
 beta = sp.constants.Boltzmann *T
 N_x = 100
 N_points = 2*N_x
+N_particles = 12*N_x
 k = 1000/beta
 
 T_p = 500
@@ -16,7 +19,6 @@ h = 1
 
 
 def V_1(x):
-
     #normaliserer x til periodisiteten
     while x > alpha*N_x:
         x = x - alpha*N_x
@@ -32,12 +34,12 @@ def V_1(x):
 def V_2(x):
     return k
 
-def random_walk_index_in_rachet(x : np.array, occupied : np.array, walker : int, V : function)
+def random_walk_particle(x : np.array, walker : int, V):
     uniform = np.random.uniform()
     step_dir = 0
-    if(uniform > p_plus(x[walker], beta, k, V(x[walker]))):
+    if(uniform > p_plus(x[walker], beta, k, V)):
         step_dir = 1
-    if(uniform  < p_minus(x[walker], beta, k, V(x[walker]))):
+    if(uniform  < p_minus(x[walker], beta, k, V)):
         step_dir = -1
 
     new_ind = walker
@@ -46,7 +48,49 @@ def random_walk_index_in_rachet(x : np.array, occupied : np.array, walker : int,
     else:
         new_ind = walker + step_dir
     
-    return new_ind
+    return new_ind, step_dir
 
+
+def random_walk_system(positions, occupied, potential):
+    n_plus = 0
+    n_minus = 0
+
+    new_occupied = np.zeros(np.shape(occupied), dtype=np.int16)
+    for index, particles  in enumerate(occupied):
+        for _ in range(int(particles)):
+            new_index, step_direction = random_walk_particle(positions ,index, potential)
+            new_occupied[new_index] += 1
+            if(step_direction == 1):
+                n_plus += 1
+                continue
+            if(step_direction == -1):
+                n_minus += 1
+                continue
+    
+    stream = (n_plus - n_minus)/N_particles
+    
+    return new_occupied, stream
+
+
+def random_walk_cycle(positions,occupied):
+    avg_stream = 0
+    for timestep in range(2*T_p):
+        potential = V_2 #antar V1 med mindre vi er i en oddetalls del av syklusen
+        if (timestep >= T_p):
+            potential = V_1
+        occupied, stream = random_walk_system(positions, occupied, potential)
+        avg_stream += stream
+
+    avg_stream /= 2*T_p
+
+    return occupied, avg_stream
+
+def oppgave_3_a(num_cycles = 10):
+    positions = np.linspace(0, h*(N_points-1), N_points)
+    occupied = np.ones((N_points),dtype=np.int16)*N_particles/N_points
+    streams = np.zeros(num_cycles)
+    for i in range(num_cycles):
+        occupied, streams[i] = random_walk_cycle(positions, occupied)
+        print(f"cycle: {i+1}, average stream {streams[i]}")
 
     
