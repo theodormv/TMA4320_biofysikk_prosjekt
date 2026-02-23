2b96a3a5b4c3ba2f383a4d57f7a841066522951b
Askeladden3 <askinatr@gmail.com>
2026-02-23 14:41:40 +0100

    småendringer

--- oppgave_2.py
+++ oppgave_2.py
@@ -10,12 +10,10 @@ def V_1(x, k):
 def V_2(x, k):
     return -k * x
 
-
-
 def random_walk(V):
     k_b = sp.constants.Boltzmann
     n_particles = 100
-    n_steps = 1
+    n_steps = 100
     h = 1
     dt = 1
     T = 273.15 + 37
@@ -30,13 +28,21 @@ def random_walk(V):
         go_left = uniform_dist <= p_minus(x_0, beta, k, V)
         go_right = uniform_dist >= 1-p_plus(x_0, beta, k, V)
         stay = (~go_left & ~go_right)
-        particles[go_left] += 1
-        particles[go_right] -= 1
+        particles[go_left] -= 1
+        particles[go_right] += 1
         dist_list.append(particles)
+
+        if i % 10 == 0:
+            print(f'p_plus: {np.average(p_plus(x_0, beta, k, V))} | p_minus: {np.average(p_minus(x_0, beta, k, V))}')
     
     return np.array(dist_list).flatten()
 
+'''
+oppg_1_a_verdier = random_walk(V_1)
+plt.hist(oppg_1_a_verdier, density=True)
+plt.show()
+'''
 
-dist_array = random_walk(V_1)
-plt.hist(dist_array, density=True)
+oppg_2_a_verdier = random_walk(V_2)
+plt.hist(oppg_2_a_verdier, density=True)
 plt.show()
