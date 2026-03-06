306377ffe8defc73bc4e43b34f9ab3aaf7a47471
H8k3y3 <haukhalv@online.no>
2026-03-06 13:01:13 +0100

    Leste gjennom oppgave 1 og 2

--- oppgaver/oppgave_2.py
+++ oppgaver/oppgave_2.py
@@ -3,15 +3,17 @@ import scipy as sp
 import matplotlib.pyplot as plt
 from utilities.probs import p_plus, p_minus
 
+#Definerer potensialene
 def V_1(x):
     return k
 
 def V_2(x):
     return - k * x
 
-
+#Lager en klasse for virrevandring
 class random_walk():
-    def __init__(self, beta_k_ratio : int, V):
+    #settere konstantene
+    def __init__(self, beta_k_ratio : int, V): 
         k_b = sp.constants.Boltzmann
         self.n_particles = 100
         self.n_walks = 200
@@ -25,6 +27,7 @@ class random_walk():
         self.oppg_a_vals = list()
         self.oppg_b_vals = list()
 
+    #Bestemmer hvilke partikkler som skal gå til venste og høyre i et steg
     def gen_walk_masks(self, particles):
         uniform_dist = np.random.uniform(0, 1, self.n_particles)
         x_0 = particles
@@ -32,6 +35,7 @@ class random_walk():
         go_right = uniform_dist >= 1-p_plus(x_0, self.beta, self.V)
         return go_left, go_right
 
+    #Setter partikklene til å gå i gitt retning, uten at de interagerer
     def oppg_a_step(self):
         particles = np.arange(0, self.n_particles, 1)
         for j in range(self.n_walks):
@@ -40,6 +44,8 @@ class random_walk():
             particles[go_right] += 1
         self.oppg_a_vals.append(particles)
     
+    #Setter partikklene til å gå i gitt retning, med at de skjekker om plassen de går er tatt
+    #De interagerer altså
     def oppg_b_step(self):
         particles = np.arange(0, self.n_particles, 1)
         for j in range(self.n_walks):
@@ -59,12 +65,14 @@ class random_walk():
         self.oppg_b_vals.append(particles)
 
 
-
+#Kjører simulasjonen
 def oppg2():
+    #Lager 2 instanser med virrevandring, en med hvert potensial.
+    #Lar så begge gå med og uten interaksjon
     n_steps = 100
 
-    walker_V1 = random_walk(beta_k_ratio=1000, V=V_1)
-    walker_V2 = random_walk(beta_k_ratio=1000, V=V_2)
+    walker_V1 = random_walk(beta_k_ratio=1, V=V_1)
+    walker_V2 = random_walk(beta_k_ratio=1, V=V_2)
 
     for j in range(n_steps):
         walker_V1.oppg_a_step()
@@ -74,7 +82,7 @@ def oppg2():
         if j % 10 == 0:
             print(f'Iteration {j} of {n_steps}')
 
-
+    #Plotter og lagrer resultatet
     for walker, potential_info in {walker_V1:('V1', 'k'), walker_V2: ('V2', '-kx')}.items():
         potential_type, potential_eq = potential_info
 
